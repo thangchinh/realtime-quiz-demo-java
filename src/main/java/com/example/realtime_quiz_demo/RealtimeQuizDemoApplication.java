b37d1b602d066eb6eff4c4d073acd054cbb4d399
thang <thang@Thangs-MacBook-Pro-M3-Pro.local>
2024-07-18 22:31:03 +0700

    init

---
+++ src/main/java/com/example/realtime_quiz_demo/RealtimeQuizDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtime_quiz_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeQuizDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeQuizDemoApplication.class, args);
+	}
+
+}
