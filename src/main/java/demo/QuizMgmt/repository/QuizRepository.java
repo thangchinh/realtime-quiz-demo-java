7c97f3ca3a5538d89252531aeae150c6291ca549
thang <thang@Thangs-MacBook-Pro-M3-Pro.local>
2024-07-20 17:17:09 +0700

    fix exception

--- src/main/java/demo/QuizMgmt/repository/QuizRepository.java
+++ src/main/java/demo/QuizMgmt/repository/QuizRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 public interface QuizRepository extends MongoRepository<Quiz, String> {
-  @Query(value = "{ 'quizId' : ?0}", fields = "{ 'quiz.quizId' : 1 }")
   List<Quiz> findByQuizId(String quizId);
 }
